2e35975ec2e722726b5ae9d8d3c6ddf61b92c6b9
Lucas Poma <lpoma2000@gmail.com>
2023-09-11 22:56:33 -0300

    ss ayb

--- src/Lang.hs
+++ src/Lang.hs
@@ -27,14 +27,18 @@ import           Data.List.Extra                ( nubSort )
 data STm info ty var =
     SV info var
   | SConst info Const
-  | SLam info (var, ty) (STm info ty var)
+  | SLam info ([(var,ty)]) (STm info ty var)
   | SApp info (STm info ty var) (STm info ty var)
   | SPrint info String (STm info ty var)
   | SBinaryOp info BinaryOp (STm info ty var) (STm info ty var)
-  | SFix info (var, ty) (var, ty) (STm info ty var)
+  | SFix info (var,ty) ([(var,ty)]) (STm info ty var)
   | SIfZ info (STm info ty var) (STm info ty var) (STm info ty var)
-  | SLet info (var, ty) (STm info ty var) (STm info ty var)
+  | SLet info Bool ([(var,ty)]) (STm info ty var) (STm info ty var)
   deriving (Show, Functor)
+
+data STy = NatTy | FunTy STy STy | TypeN Name
+
+data SDecl info = SDType info Name STy | SDDecl info Bool ([(Name,STy)]) Name STerm
 
 -- | AST de Tipos
 data Ty =
