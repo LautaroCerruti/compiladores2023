acf677f7b46a0d127767d1a60bbe3e712bfe2515
Lucas Poma <lpoma2000@gmail.com>
2023-09-13 23:20:55 -0300

    start ss punto d

--- src/Elab.hs
+++ src/Elab.hs
@@ -14,30 +14,54 @@ module Elab ( elab, elabDecl) where
 
 import Lang
 import Subst
+import MonadFD4
 
 -- | 'elab' transforma variables ligadas en índices de de Bruijn
 -- en un término dado. 
-elab :: STerm -> Term
+elab :: MonadFD4 m => STerm -> m Term
 elab = elab' []
 
-elab' :: [Name] -> STerm -> Term
+elab' :: MonadFD4 m => [Name] -> STerm -> m Term
 elab' env (SV p v) =
   -- Tenemos que ver si la variable es Global o es un nombre local
   -- En env llevamos la lista de nombres locales.
-  if v `elem` env 
-    then  V p (Free v)
-    else V p (Global v)
+  return $ if v `elem` env 
+            then V p (Free v)
+            else V p (Global v)
 
-elab' _ (SConst p c) = Const p c
-elab' env (SLam p (v,ty) t) = Lam p v ty (close v (elab' (v:env) t))
+elab' _ (SConst p c) = return $ Const p c
+
+elab' env (SLam p [] t) = failPosFD4 p "Elab: Funcion sin Argumentos"
+elab' env (SLam p [(v,ty)] t) = do e <- elab' (v:env) t 
+                                   return $ Lam p v ty (close v e)
+elab' env (SLam p ((v,ty):vs) t) = do e <- elab' (v:env) (SLam p vs t) 
+                                      return $ Lam p v ty (close v e)
+
+
+
+-- WIP
 elab' env (SFix p (f,fty) (x,xty) t) = Fix p f fty x xty (close2 f x (elab' (x:f:env) t))
-elab' env (SIfZ p c t e)         = IfZ p (elab' env c) (elab' env t) (elab' env e)
+
+
+
+elab' env (SIfZ p c t e) = do c' <- elab' env c
+                              t' <- elab' env t
+                              e' <- elab' env e 
+                              return $ Ifz p c' t' e'
 -- Operadores binarios
-elab' env (SBinaryOp i o t u) = BinaryOp i o (elab' env t) (elab' env u)
--- Operador Print
-elab' env (SPrint i str t) = Print i str (elab' env t)
+elab' env (SBinaryOp i o t u) = do t' <- elab' env t
+                                   u' <= elab' env u 
+                                   return $ BinaryOp i o t' u'
+-- Operador Print (VER FD4)
+elab' env (SPrint i str t) = do t' <= elab' env t
+                                return $ Print i str t' 
 -- Aplicaciones generales
-elab' env (SApp p h a) = App p (elab' env h) (elab' env a)
+elab' env (SApp p h a) = do h' <- elab' env h
+                            a' <- elab' env a
+                            return $ App p h' a'
+
+
+-- WIP
 elab' env (SLet p (v,vty) def body) =  
   Let p v vty (elab' env def) (close v (elab' (v:env) body))
 
